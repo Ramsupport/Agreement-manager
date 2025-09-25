db9711fc3a7cd50207ae0a00b98db519c9889b1f
Ramsupport <support@ramnathshetty.com>
2025-09-25 07:41:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+const path = require('path');
+app.use(express.static(path.join(__dirname)));
 
 // Database connection
 const pool = new Pool({
